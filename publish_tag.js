fccdf70fe0a6450fd5a21d17415a2e1b52399939
Dongqi Guo <dongqi.guo@compass.com>
2022-05-16 18:47:17 +0800

    a

--- publish_tag.js
+++ publish_tag.js
@@ -10,12 +10,14 @@ const [pull_number, token] = args;
 
 console.log(pull_number, token);
 
-// const octokit = new Octokit({
-//   auth: 'personal-access-token123'
-// })
+const octokit = new Octokit({
+  auth: '09b1c87f0252b18fd5e6e3e211f4a456df3642d1'
+})
 
-// await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
-//   owner: 'OWNER',
-//   repo: 'REPO',
-//   pull_number: 'PULL_NUMBER'
-// })
+const pr = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
+  owner: 'guodongqi-boy',
+  repo: 'todo-mvc',
+  pull_number: 12
+});
+
+console.log(pr);
